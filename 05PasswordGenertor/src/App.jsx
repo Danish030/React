539fe683b9661710206343976373634648b5c581
Danish <danish58278@gmail.com>
2024-09-10 10:08:49 +0530

    05 project

---
+++ 05PasswordGenertor/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+ 
+
+  return (
+    <>
+<div className="hello">
+<h1 className="text-4xl text-white">Password Generator</h1>
+
+</div>
+    </>
+  )
+}
+
+export default App
